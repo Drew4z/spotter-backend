f1a89fbd9c929182438cddf08fd55877c4b96997
Andrew <andrewwac27@gmail.com>
2026-01-26 09:59:24 +0100

    base commit

---
+++ src/main/java/com/spotter_proyect/spotter/SpotterApplication.java
@@ -0,0 +1,13 @@
+package com.spotter_proyect.spotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotter_proyect/spotter/SpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotter_proyect.spotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
